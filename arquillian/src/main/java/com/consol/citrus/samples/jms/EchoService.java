d1e6828f2d004fba2f3c180009c0d5e8e74e67a0
christophd <deppisch@consol.de>
2015-03-09 17:05:28 +0100

    Add Arquillian JMS test with Citrus integration

---
+++ arquillian/src/main/java/com/consol/citrus/samples/jms/EchoService.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2006-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.samples.jms;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.*;
+
+/**
+ * @author Christoph Deppisch
+ * @since 2.2
+ */
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty( propertyName = "destination", propertyValue = "jms/queue/test"),
+        @ActivationConfigProperty( propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class EchoService implements MessageListener {
+
+    @Resource(mappedName = "java:/ConnectionFactory")
+    private ConnectionFactory factory;
+
+    @Override
+    public void onMessage(Message msg) {
+        try
+        {
+            System.out.println("Received JMS request " + msg.getJMSMessageID());
+
+            Connection connection = factory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer producer = session.createProducer(msg.getJMSReplyTo());
+            producer.send(msg);
+            producer.close();
+            session.close();
+            connection.close();
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Could not reply to JMS message", e);
+        }
+    }
+}
---
+++ arquillian/src/test/java/com/consol/citrus/samples/jms/EchoServiceTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2006-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.samples.jms;
+
+import com.consol.citrus.Citrus;
+import com.consol.citrus.config.CitrusBaseConfig;
+import com.consol.citrus.dsl.CitrusTestBuilder;
+import com.consol.citrus.jms.endpoint.JmsSyncEndpoint;
+import com.consol.citrus.jms.endpoint.JmsSyncEndpointConfiguration;
+import com.consol.citrus.jms.message.JmsMessage;
+import com.consol.citrus.message.MessageType;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.*;
+import org.junit.runner.RunWith;
+
+import javax.annotation.Resource;
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+import java.net.MalformedURLException;
+
+@RunWith(Arquillian.class)
+public class EchoServiceTest {
+
+    private Citrus citrusFramework = Citrus.newInstance(CitrusBaseConfig.class);
+    private CitrusTestBuilder citrus;
+    private JmsSyncEndpoint jmsSyncEndpoint;
+
+    @Deployment
+    public static WebArchive createDeployment() throws MalformedURLException {
+        return ShrinkWrap.create(WebArchive.class)
+                .addClasses(EchoService.class)
+                .addAsLibraries(Maven.configureResolver()
+                        .workOffline()
+                        .resolve("com.consol.citrus:citrus-core:2.2",
+                                "com.consol.citrus:citrus-jms:2.2",
+                                "com.consol.citrus:citrus-java-dsl:2.2")
+                        .withTransitivity()
+                        .asFile());
+    }
+
+    @Resource(mappedName = "jms/queue/test")
+    private Queue echoQueue;
+
+    @Resource(mappedName = "/ConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+    @Before
+    public void setUp() {
+        Assert.assertTrue(connectionFactory != null);
+        JmsSyncEndpointConfiguration endpointConfiguration = new JmsSyncEndpointConfiguration();
+        endpointConfiguration.setConnectionFactory(connectionFactory);
+        endpointConfiguration.setDestination(echoQueue);
+        jmsSyncEndpoint = new JmsSyncEndpoint(endpointConfiguration);
+        citrus = new CitrusTestBuilder(citrusFramework.getApplicationContext());
+    }
+
+    @Test
+    public void shouldBeAbleToSendMessage() throws Exception {
+        citrus.name("EchoServiceTest");
+
+        String messageBody = "ping";
+        citrus.send(jmsSyncEndpoint)
+                .messageType(MessageType.PLAINTEXT)
+                .message(new JmsMessage(messageBody));
+
+        citrus.receive(jmsSyncEndpoint)
+                .messageType(MessageType.PLAINTEXT)
+                .message(new JmsMessage(messageBody));
+
+        citrusFramework.run(citrus.getTestCase());
+    }
+}
