0000866ea8b72cfbca9711aabc3403df7cfa6468
christophd <deppisch@consol.de>
2015-10-03 17:31:09 +0200

    Enable multiple worker war deployments in a single jetty server

---
+++ camel-bakery/bakery-worker/src/main/java/com/consol/citrus/samples/bakery/servlet/InitializeFactoryListener.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2006-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.samples.bakery.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @author Christoph Deppisch
+ * @since 2.4
+ */
+public class InitializeFactoryListener implements ServletContextListener {
+
+    /** Logger */
+    private static Logger log = LoggerFactory.getLogger(InitializeFactoryListener.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        if (servletContextEvent.getServletContext().getContextPath().startsWith("/worker-")) {
+            log.info("Setting factory type using servlet context path '{}'", servletContextEvent.getServletContext().getContextPath());
+            System.setProperty("FACTORY_TYPE", servletContextEvent.getServletContext().getContextPath().substring("/worker-".length()));
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+    }
+}
