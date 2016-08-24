17decfaf9f075ca49e425e682269465856cb8da8
christophd <deppisch@consol.de>
2016-08-24 16:34:46 +0200

    Add new sample for XHTML validation

---
+++ sample-xhtml/src/main/java/com/consol/citrus/samples/todolist/model/TodoEntry.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2006-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.samples.todolist.model;
+
+import java.util.UUID;
+
+/**
+ * @author Christoph Deppisch
+ */
+public class TodoEntry {
+
+    private UUID id;
+    private String title;
+    private String description;
+
+    public TodoEntry() {
+    }
+
+    public TodoEntry(String title, String description) {
+        this(UUID.randomUUID(), title, description);
+    }
+
+    public TodoEntry(UUID id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ sample-xhtml/src/test/java/com/consol/citrus/samples/todolist/TodoListIT.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2006-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.samples.todolist;
+
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.message.MessageType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.Test;
+
+/**
+ * @author Christoph Deppisch
+ */
+public class TodoListIT extends TestNGCitrusTestDesigner {
+
+    @Autowired
+    private HttpClient todoClient;
+
+    @Test
+    @CitrusTest
+    public void testIndexPage() {
+        http()
+            .client(todoClient)
+            .get("/");
+
+        http()
+            .client(todoClient)
+            .response(HttpStatus.OK)
+            .messageType(MessageType.XHTML)
+            .xpath("//xh:h1", "TODO list")
+            .payload("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n" +
+                    "\"org/w3/xhtml/xhtml1-transitional.dtd\">" +
+                    "<html xmlns=\"http://www.w3.org/1999/xhtml\">" +
+                        "<head>@ignore@</head>" +
+                        "<body>@ignore@</body>" +
+                    "</html>");
+    }
+
+    @Test
+    @CitrusTest
+    public void testAddTodoEntry() {
+        variable("todoName", "citrus:concat('todo_', citrus:randomNumber(4))");
+        variable("todoDescription", "Description: ${todoName}");
+
+        http()
+            .client(todoClient)
+            .post("/")
+            .contentType("application/x-www-form-urlencoded")
+            .payload("title=${todoName}&description=${todoDescription}");
+
+        http()
+            .client(todoClient)
+            .response(HttpStatus.FOUND);
+
+        http()
+            .client(todoClient)
+            .get();
+
+        http()
+            .client(todoClient)
+            .response(HttpStatus.OK)
+            .messageType(MessageType.XHTML)
+            .xpath("(//xh:li[@class='list-group-item'])[last()]", "${todoName}");
+    }
+
+}
