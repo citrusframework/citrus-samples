0b07bb3b9b9de5d3a0ba8042c2cd469a8c4ab7db
christophd <deppisch@consol.de>
2016-08-24 11:13:14 +0200

    Add new sample project for javaconfig

---
+++ sample-javaconfig/README.md
@@ -0,0 +1,47 @@
+Citrus samples ![Logo][1]
+==============
+
+Java config sample
+---------
+
+This sample uses pure Java configuration for Citrus. No XML files all pure Java.
+  
+Server
+---------
+
+Got to the folder and start the BookStore WebService application in a Web Container. Easiest
+way for you to do this is to execute
+
+> mvn jetty:run
+
+here!
+
+An embedded Jetty Web Server Container is started with the BookStore application deployed. You can
+alsp call "mvn package" and deploy the resulting war archive to a separate Web container of your choice.
+  
+Citrus test
+---------
+
+Once the sample application is deployed and running you can execute the Citrus test cases.
+Open a separate command line terminal and navigate to the sample folder.
+
+Execute all Citrus tests by calling
+
+> mvn integration-test
+
+You can also pick a single test by calling
+
+> mvn integration-test -Ptest=TestName
+
+You should see Citrus performing several tests with lots of debugging output in both terminals (sample application server
+and Citrus test client). And of course green tests at the very end of the build.
+
+Information
+---------
+
+For more information on Citrus see [www.citrusframework.org][2], including
+a complete [reference manual][3].
+
+ [1]: http://www.citrusframework.org/img/brand-logo.png "Citrus"
+ [2]: http://www.citrusframework.org
+ [3]: http://www.citrusframework.org/reference/html/
---
+++ sample-javaconfig/src/test/java/com/consol/citrus/samples/todolist/TodoListIT.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2006-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.samples.todolist;
+
+import com.consol.citrus.annotations.CitrusEndpoint;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.http.config.annotation.HttpClientConfig;
+import com.consol.citrus.message.MessageType;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.Test;
+
+/**
+ * @author Christoph Deppisch
+ */
+@ContextConfiguration(classes = { EndpointConfig.class })
+public class TodoListIT extends TestNGCitrusTestDesigner {
+
+    @CitrusEndpoint
+    @HttpClientConfig(requestUrl = "http://localhost:8080")
+    private HttpClient todoClient;
+
+    @Test
+    @CitrusTest
+    public void testGet() {
+        http()
+            .client(todoClient)
+            .get("/");
+
+        http()
+            .client(todoClient)
+            .response(HttpStatus.OK)
+            .messageType(MessageType.XHTML)
+            .namespace("xh", "http://www.w3.org/1999/xhtml")
+            .xpath("//xh:h1", "TODO list")
+            .payload("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n" +
+                    "\"org/w3/xhtml/xhtml1-transitional.dtd\">" +
+                    "<html xmlns=\"http://www.w3.org/1999/xhtml\">" +
+                        "<head>@ignore@</head>" +
+                        "<body>@ignore@</body>" +
+                    "</html>");
+    }
+
+}
