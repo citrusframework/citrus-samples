67ec470c9dab8c4e1e0dba03835cfa4478b6ac12
christophd <deppisch@consol.de>
2016-09-08 09:55:02 +0200

    Use todo-app as system under test in cucumber spring sample

--- sample-cucumber-spring/src/test/java/todo/TodoFeatureIT.java
+++ sample-cucumber-spring/src/test/java/todo/TodoFeatureIT.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package echo;
+package todo;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -25,6 +25,6 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = { "com.consol.citrus.cucumber.CitrusReporter" })
-public class EchoFeatureIT {
+        plugin = { "com.consol.citrus.cucumber.CitrusReporter" } )
+public class TodoFeatureIT {
 }
