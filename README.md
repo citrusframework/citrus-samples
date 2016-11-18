d888204acee9d9482470acf0b37478465c4aa1d9
christophd <deppisch@consol.de>
2016-11-18 16:10:34 +0100

    Add new load testing sample using Http

--- README.md
+++ README.md
@@ -32,6 +32,7 @@ system under test. Please find following list of samples and their primary objec
 | [sample-dynamic-endpoints](sample-dynamic-endpoints)| Shows dynamic endpoint component usage |
 | [sample-jms](sample-jms)| Shows JMS queue connectivity |
 | [sample-http](sample-http)| Shows Http REST API calls as a client |
+| [sample-http-loadtest](sample-http-loadtest)| Calls REST API on Http server with multiple threads for load testing |
 | [sample-http-static-response](sample-http-static-response)| Shows how to setup a static response generating Http server component |
 | [sample-https](sample-https)| Shows how to use SSL connectivity as a client and server |
 | [sample-soap](sample-soap)| Shows SOAP web service support |
---
+++ sample-http-loadtest/src/test/java/com/consol/citrus/samples/todolist/TodoListLoadTestIT.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2006-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.samples.todolist;
+
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.dsl.design.TestDesigner;
+import com.consol.citrus.dsl.testng.TestNGCitrusTest;
+import com.consol.citrus.http.client.HttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.*;
+
+/**
+ * @author Christoph Deppisch
+ */
+@Test(invocationCount = 250, threadPoolSize = 25)
+public class TodoListLoadTestIT extends TestNGCitrusTest {
+
+    @Autowired
+    private HttpClient todoClient;
+
+    @Parameters( { "designer" })
+    @CitrusTest
+    public void testAddTodo(@Optional @CitrusResource TestDesigner designer) {
+        designer.http()
+            .client(todoClient)
+            .send()
+            .post("/todolist")
+            .contentType("application/x-www-form-urlencoded")
+            .payload("title=citrus:concat('todo_', citrus:randomNumber(10))");
+
+        designer.http()
+            .client(todoClient)
+            .receive()
+            .response(HttpStatus.FOUND);
+    }
+
+    @Parameters( { "designer" })
+    @CitrusTest
+    public void testListTodos(@Optional @CitrusResource TestDesigner designer) {
+        designer.http()
+            .client(todoClient)
+            .send()
+            .get("/todolist")
+            .accept("text/html");
+
+        designer.http()
+            .client(todoClient)
+            .receive()
+            .response(HttpStatus.OK);
+    }
+
+}
